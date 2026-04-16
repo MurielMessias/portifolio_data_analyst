47a91988d49f84f06af77137aac4790e4efe29e1
DarkSidarael <155545873+DarkSidarael@users.noreply.github.com>
2026-04-15 22:11:47 -0300

    forcando deploy da pasta dist compilada

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/portifolio_data_analyst/', // Isso aqui é a chave!
 });
