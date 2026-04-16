902435ecb58077d92bfb4aa1caf101df83a9bbbc
DarkSidarael <155545873+DarkSidarael@users.noreply.github.com>
2026-04-15 22:23:23 -0300

    resolvendo erro de mime type e paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // ISSO AQUI É O QUE CORRIGE O 404
+  // Nome exato do seu repositório no GitHub
   base: '/portifolio_data_analyst/',
   build: {
     emptyOutDir: true,
