e7a111e8078bd2b1315678daf5fe103d01bbe5f9
DarkSidarael <155545873+DarkSidarael@users.noreply.github.com>
2026-04-15 22:04:33 -0300

    corrigindo caminhos de build

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // O ponto antes da barra é o segredo para o GitHub Pages
   base: './', 
-  build: {
-    // Garante que o build limpe a pasta antes de gerar novos arquivos
-    emptyOutDir: true,
-  }
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
