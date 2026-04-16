72e7d64ec46754661054efa5481d3b2063898617
DarkSidarael <155545873+DarkSidarael@users.noreply.github.com>
2026-04-15 21:49:10 -0300

    fix: ajuste de MIME type e deploy oficial

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Use o caminho fixo do seu repositório
-  base: '/portifolio_data_analyst/',
   plugins: [react()],
+  base: '/portifolio_data_analyst/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
