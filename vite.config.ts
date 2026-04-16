550a69ace3829fa2a83263d9be47e09ef2de2613
DarkSidarael <155545873+DarkSidarael@users.noreply.github.com>
2026-04-15 22:15:02 -0300

    fix: explicit base and relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portifolio_data_analyst/', // Isso aqui é a chave!
+  // ISSO AQUI É O QUE CORRIGE O 404
+  base: '/portifolio_data_analyst/',
+  build: {
+    emptyOutDir: true,
+  }
 });
