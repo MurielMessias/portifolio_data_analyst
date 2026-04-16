b32e16c5647d3d2fea9e88523ea335496ce20ca5
DarkSidarael <155545873+DarkSidarael@users.noreply.github.com>
2026-04-15 21:29:22 -0300

    limpando base url e corrigindo index

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-const basePath = process.env.VITE_BASE_URL || '/portifolio_data_analyst/';
-
 export default defineConfig({
-  base: basePath,
+  // Use o caminho fixo do seu repositório
+  base: '/portifolio_data_analyst/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
